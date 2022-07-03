74a3821a0bcebef203ab5111be7a0333ef2c6f97
Mindi Weik <mindiweik@gmail.com>
2022-07-03 08:40:19 -0600

    Completed 1-6

--- src/recursion.js
+++ src/recursion.js
@@ -1,37 +1,91 @@
-/* jshint esversion: 6 */
-
-// Solve the following prompts using recursion.
-
-// 1. Calculate the factorial of a number. The factorial of a non-negative integer n,
-// denoted by n!, is the product of all positive integers less than or equal to n.
-// Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
-// factorial(5); // 120
 var factorial = function(n) {
+  if (n < 0) {
+    return null;
+  }
+
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 };
 
-// 2. Compute the sum of an array of integers.
-// sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+
+  if (array.length === 1) {
+    return array[0];
+  } else {
+    return array[0] + sum(array.slice(1));
+  }
 };
 
-// 3. Sum all numbers in an array containing nested arrays.
-// arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  var sum = 0;
+  if (array.length === 0) {
+    return 0;
+  }
+
+  if (typeof array === 'number') {
+    sum += array;
+  }
+  if (Array.isArray(array)) {
+    array.forEach(function(val) {
+      sum += arraySum(val);
+    });
+  }
+  return sum;
 };
 
-// 4. Check if a number is even.
 var isEven = function(n) {
+  if (n < 0) {
+    n = n * -1;
+  }
+
+  if (n === 0) {
+    return true;
+  } else if (n === 1) {
+    return false;
+  } else {
+    return isEven(n - 2);
+  }
 };
 
-// 5. Sum all integers below a given integer.
-// sumBelow(10); // 45
-// sumBelow(7); // 21
 var sumBelow = function(n) {
+  var isNegative = false;
+  if (n < 0) {
+    isNegative = true;
+    n *= -1;
+  }
+
+  if (n === 0 || n === 1) {
+    return 0;
+  }
+
+  if (n > 1) {
+    n--;
+    if (isNegative) {
+      n *= -1;
+    }
+    return n + sumBelow(n);
+  }
 };
 
-// 6. Get the integers within a range (x, y).
-// range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+  if (x === y - 1 || x === y + 1) {
+    return [];
+  }
+  if (x === y) {
+    return [];
+  }
+
+  if (x < y) {
+    return [x + 1].concat(range(x + 1, y));
+  } else {
+    return [x - 1].concat(range(x - 1, y));
+  }
 };
 
 // 7. Compute the exponent of a number.
